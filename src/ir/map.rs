66aa3fa25811597a847d70439704810bf0b1b19b
John DiSanti <johndisanti@gmail.com>
2017-12-23 21:54:35 -0800

    Add more information to the source maps

--- src/ir/map.rs
+++ src/ir/map.rs
@@ -1,52 +1,57 @@
-use std::collections::BTreeMap;
-
 use ir::{IRBlock, IRChunk, IROp, IR};
-use src_tag::SrcTag;
 use src_unit::SrcUnits;
+
+#[derive(Serialize)]
+pub struct SourceMapEntry {
+    unit: usize,
+    offset: usize,
+    line: usize,
+    address: u16,
+}
 
 #[derive(Serialize)]
 pub struct SourceMap<'a> {
     src_units: &'a SrcUnits,
-    pc_to_src: BTreeMap<u16, SrcTag>,
+    entries: Vec<SourceMapEntry>,
 }
 
 impl<'a> SourceMap<'a> {
     pub fn new(src_units: &'a SrcUnits, ir: &IR) -> SourceMap<'a> {
         let mut map = SourceMap {
             src_units: src_units,
-            pc_to_src: BTreeMap::new(),
+            entries: Vec::new(),
         };
 
-        ir.add_entries(&mut map);
+        ir.add_entries(src_units, &mut map);
         map
     }
 }
 
 trait AddEntries {
-    fn add_entries(&self, source_map: &mut SourceMap);
+    fn add_entries(&self, src_units: &SrcUnits, source_map: &mut SourceMap);
 }
 
 impl AddEntries for IR {
-    fn add_entries(&self, source_map: &mut SourceMap) {
+    fn add_entries(&self, src_units: &SrcUnits, source_map: &mut SourceMap) {
         for block in &self.blocks {
-            block.add_entries(source_map);
+            block.add_entries(src_units, source_map);
         }
     }
 }
 
 impl AddEntries for IRBlock {
-    fn add_entries(&self, source_map: &mut SourceMap) {
+    fn add_entries(&self, src_units: &SrcUnits, source_map: &mut SourceMap) {
         for chunk in &self.chunks {
-            chunk.add_entries(source_map);
+            chunk.add_entries(src_units, source_map);
         }
     }
 }
 
 impl AddEntries for IRChunk {
-    fn add_entries(&self, source_map: &mut SourceMap) {
+    fn add_entries(&self, src_units: &SrcUnits, source_map: &mut SourceMap) {
         match *self {
             IRChunk::Op(ref op) => {
-                op.add_entries(source_map);
+                op.add_entries(src_units, source_map);
             }
             _ => {}
         }
@@ -54,7 +59,14 @@ impl AddEntries for IRChunk {
 }
 
 impl AddEntries for IROp {
-    fn add_entries(&self, source_map: &mut SourceMap) {
-        source_map.pc_to_src.insert(self.position, self.tag);
+    fn add_entries(&self, src_units: &SrcUnits, source_map: &mut SourceMap) {
+        source_map.entries.push(SourceMapEntry {
+            unit: self.tag.unit,
+            offset: self.tag.offset,
+            line: (&src_units.unit(self.tag.unit).source[0..self.tag.offset])
+                .chars()
+                .fold(1, |acc, chr| if chr == '\n' { acc + 1 } else { acc }),
+            address: self.position,
+        });
     }
 }
